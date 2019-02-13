157c628bca27bcb61c49bc0b67507f4c325756ee
nanashinogonbee <vgpro80@gmail.com>
2019-02-13 17:36:18 +0300

    First stable version

--- main.py
+++ main.py
@@ -47,9 +47,19 @@ data = {
 
 print('started')
 
-
 @bot.message_handler(commands=['start'])
 def start(message):
+    bot.send_message(
+                     message.chat.id,
+                     'Привет! Я - бот, который может вывести посещаемость '
+                     'студентов 2 курса ИВТ в РГПУ им. Герцена. Чтоб это '
+                     'сделать, отправь команду `/stats`.',
+                     parse_mode='markdown'
+                     )
+
+
+@bot.message_handler(commands=['stats'])
+def stats(message):
     buttons = []
     markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True)
     for i in months:
@@ -57,9 +67,7 @@ def start(message):
     markup.add(*buttons)
     msg = bot.send_message(
                      message.chat.id,
-                     'Привет! Я - бот, который может вывести посещаемость '
-                     'студентов 2 курса ИВТ в РГПУ им. Герцена. Для начала '
-                     'выбери месяц, нажав на одну из кнопок.',
+                     'Выбери месяц, нажав на одну из кнопок.',
                      reply_markup=markup
                      )
     bot.register_next_step_handler(msg, monthpick)
@@ -86,14 +94,40 @@ def datepick(message):
     data['picked_date'] = parseint(message.text)
     if data['picked_date'] > 0:
         if data['picked_date'] < 28 + month_offset[data['picked_month']]:
+            res = ''
             sel_sheet = sheet.get_worksheet(data['picked_month'])
             sel_column = sel_sheet.find(str(data['picked_date'])).col
             sel_column = string.ascii_uppercase[sel_column - 1]
-            cell_list = sel_sheet.range(f'{sel_column}1:{sel_column}38')
-            cell_vals = [i.value for i in cell_list]
+            att_list = sel_sheet.range(f'{sel_column}1:{sel_column}38')
+            att_vals = [i.value for i in att_list]
+            stud_list = sel_sheet.range(f'A1:A38')
+            stud_vals = [i.value for i in stud_list]
+            classes = 0
+            # print(att_vals)
+            for i in range(1, len(stud_list)):
+                attendance = ''
+                if att_vals[i].isdigit() and att_vals[i]:
+                    classes = parseint(att_vals[i])
+                for char in att_vals[i]:
+                    if char == 'п':
+                        attendance = classes * '⭕️'
+                        break
+                    elif char == 'о':
+                        attendance = classes * '❌'
+                        break
+                    elif char.isdigit() and int(char) < classes:
+                        attendance += (int(char) * '⭕️')
+                    elif char == '-':
+                        attendance += '❌'
+                    elif char == '':
+                        attendance = ''
+                    else:
+                        pass
+                if stud_vals[i] not in ['День недели', 'Количество пар']:
+                    res += f'{stud_vals[i]} {attendance}\n'
             bot.send_message(
                      message.chat.id,
-                     '|'.join(cell_vals)
+                     res
                      )
         else:
             bot.send_message(
